a986d89f7c0854df71c881e212818dde9d7d9f4f
Harneeetah <harneeetah@gmail.com>
2024-09-19 16:03:02 +0100

    new

---
+++ script.js
@@ -0,0 +1,4 @@
+// listens to the event on the nav(breadcrumbs)
+// get current url
+console.log(window.location.href);
+// compare with the attibute of the href and the current url and if they match add the attribute aria-current="page"
